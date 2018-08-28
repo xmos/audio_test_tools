79753edf269ad16e4e537b29c585deb67799a484
Oscar Bailey <oscarbailey@xmos.com>
2018-08-28 15:14:26 +0100

    Add SYSTEM_DELAY_SAMPLES, change length -> duration, add exception to get_h

--- python/audio_generation.py
+++ python/audio_generation.py
@@ -4,99 +4,106 @@ import scipy.signal
 import scipy.io.wavfile
 import numpy as np
 
-DEFAULT_SAMPLE_RATE=16000
+DEFAULT_SAMPLE_RATE = 16000
+SYSTEM_DELAY_SAMPLES = 40
 
-def reverb_filter(length_ms, amplitude, delay_ms,
+def reverb_filter(duration_ms, amplitude, delay_ms,
                          sample_rate=DEFAULT_SAMPLE_RATE):
     """ Generates the impulse response for a reverberation.
     The amplitude parameter should be < 1. Larger amplitude = longer reverb."""
     delay = int(sample_rate * delay_ms / 1000)
-    signal = np.zeros((sample_rate * length_ms / 1000, ))
-    signal[40] = 1
-    for i in range(40, sample_rate * length_ms / 1000, delay):
+    signal = np.zeros((sample_rate * duration_ms / 1000, ))
+    signal[SYSTEM_DELAY_SAMPLES] = 1
+    for i in range(SYSTEM_DELAY_SAMPLES, sample_rate*duration_ms / 1000, delay):
         delay_i = i + delay
-        if delay_i >= sample_rate * length_ms / 1000:
+        if delay_i >= sample_rate * duration_ms / 1000:
             break
         signal[delay_i] = signal[i] * amplitude
     return signal
 
 
-def echo_filter(length_ms, amplitude, delay_ms,
+def echo_filter(duration_ms, amplitude, delay_ms,
                 sample_rate=DEFAULT_SAMPLE_RATE):
     """ Generates an echo impulse response. """
     delay = int(sample_rate * delay_ms / 1000)
-    signal = np.zeros((sample_rate * length_ms / 1000, ))
-    signal[40] = 1
+    signal = np.zeros((sample_rate * duration_ms / 1000, ))
+    signal[SYSTEM_DELAY_SAMPLES] = 1
     signal[delay] = amplitude
     return signal
 
 
-def get_noise(length=None, samples=None, db=0, sample_rate=DEFAULT_SAMPLE_RATE):
+def get_noise(duration=None, samples=None, db=0,
+              sample_rate=DEFAULT_SAMPLE_RATE):
     """ Generates white noise, useful for generating background noise.
     Set dB to a large negative value (e.g. -150) to generate background
-    noise."""
-    if length:
-        samples = length*sample_rate
+    noise.
+    Either specify a duration in seconds, or number of samples."""
+    if duration:
+        samples = duration*sample_rate
     x = np.random.normal(size=(samples,))
     factor = np.power(10, db / 20.0)
     y = x * factor
     return y
 
 
-def get_h(h_type, normalise=True):
+def get_h(h_type='short', normalise=True):
     """ Generates a transfer function """
     if h_type == 'short':
         h = echo_filter(200, 0.7, 40)
-    if h_type == 'long':
+    elif h_type == 'long':
         h = echo_filter(200, 0.7, 170)
-    if h_type == 'excessive':
+    elif h_type == 'excessive':
         h = echo_filter(200, 0.7, 190)
-    if h_type == 'decaying':
+    elif h_type == 'decaying':
         h = reverb_filter(500, -0.9, 12)
+    else:
+        raise Exception("h_type invalid")
+
     if normalise:
         h = h / np.sum(np.abs(h))
     return h
     raise Exception("H type '%s' not valid" % h_type)
 
 
-def get_sine(length, frequencies, sample_rate=DEFAULT_SAMPLE_RATE, rshift=0):
+def get_sine(duration, frequencies, sample_rate=DEFAULT_SAMPLE_RATE, rshift=0):
     """ Generates a signal containing one or more sine waves of constant
     frequency.
-    Frequencies is a list of frequencies """
-    x = np.linspace(0, length * 2 * np.pi, length * sample_rate)
-    signal = np.zeros((length * sample_rate,))
+    Duration is in seconds.
+    Frequencies is a list of frequencies. """
+    x = np.linspace(0, duration * 2 * np.pi, duration * sample_rate)
+    signal = np.zeros((duration * sample_rate,))
     for freq in frequencies:
         signal += np.sin(freq * x)
     return signal / (1<<rshift)
 
 
-def get_near_end(length, frequencies=[700], sample_rate=DEFAULT_SAMPLE_RATE,
+def get_near_end(duration, frequencies=[700], sample_rate=DEFAULT_SAMPLE_RATE,
                  rshift=4):
     """ Gets a near-end signal (alias for get_sine) """
-    return get_sine(length, frequencies, sample_rate, rshift)
+    return get_sine(duration, frequencies, sample_rate, rshift)
 
 
-def get_ref_discrete(length, freq_a=1000, freq_b=2000, period=1,
+def get_ref_discrete(duration, freq_a=1000, freq_b=2000, period=1,
                      sample_rate=DEFAULT_SAMPLE_RATE, rshift=0):
     """ Gets a reference signal which oscillates between two frequencies
 
     The signal produced will have magnitude in the frequency domain at only
     those two frequencies."""
-    x = np.linspace(0, length * 2 * np.pi, length * sample_rate)
+    x = np.linspace(0, duration * 2 * np.pi, duration * sample_rate)
     y_1 = np.sin(freq_a * x)
     y_2 = np.sin(freq_b * x)
     signal = np.sin(x / (period*2))**2 * y_1 + np.cos(x / (period*2))**2 * y_2
     return signal / (1<<rshift)
 
 
-def get_ref_continuous(length, freq_a=500, freq_b=4000, period=0.2,
+def get_ref_continuous(duration, freq_a=500, freq_b=4000, period=0.2,
                        sample_rate=DEFAULT_SAMPLE_RATE, rshift=0):
     """ Gets a reference signal which oscillates smoothly between two
     frequencies
 
     The signal produced will have magnitude in the frequency domain at all
     frequencies between the two frequencies."""
-    x = np.linspace(0, length * 2 * np.pi, length * sample_rate)
+    x = np.linspace(0, duration * 2 * np.pi, duration * sample_rate)
     f = np.sin(x/period)*(freq_b-freq_a)/2 + (freq_a+freq_b)/2
     y = np.cumsum(f) / sample_rate * 2 * np.pi
     z = np.sin(y)
@@ -104,16 +111,16 @@ def get_ref_continuous(length, freq_a=500, freq_b=4000, period=0.2,
     return signal / (1<<rshift)
 
 
-def get_ref(length, ref='continuous', sample_rate=DEFAULT_SAMPLE_RATE):
+def get_ref(duration, ref='continuous', sample_rate=DEFAULT_SAMPLE_RATE):
     """ Generates a reference signal """
     if ref == "continuous":
-        return get_ref_continuous(length, sample_rate=sample_rate)
+        return get_ref_continuous(duration, sample_rate=sample_rate)
     if ref == "discrete":
-        return get_ref_discrete(length, sample_rate=sample_rate)
+        return get_ref_discrete(duration, sample_rate=sample_rate)
     if ref == "single":
-        return get_sine(length, frequencies=[1000], sample_rate=sample_rate)
+        return get_sine(duration, frequencies=[1000], sample_rate=sample_rate)
     if ref == "noise":
-        return get_noise(length, sample_rate=sample_rate, db=0)
+        return get_noise(duration, sample_rate=sample_rate, db=0)
 
 
 def get_headroom_divisor(data, headroom):
