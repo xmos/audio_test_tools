96ca075f1d0e88d89085517dcd066b9c4211f592
Andrew Stanford-Jason <andrew@xmos.com>
2018-09-04 11:29:08 +0100

    updated python setup to source mode python folders

--- python/python_setup.sh
+++ python/python_setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export PYTHONPATH=$PYTHONPATH:`pwd`:`dirname $BASH_SOURCE`
+export PYTHONPATH=$PYTHONPATH:`pwd`:`dirname $BASH_SOURCE`:`dirname $BASH_SOURCE`/../../lib_vad/python:`dirname $BASH_SOURCE`/../../lib_aec/python:`dirname $BASH_SOURCE`/../../lib_beamsteering/python
