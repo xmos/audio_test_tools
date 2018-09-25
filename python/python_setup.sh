6e429f2f4085c539706b61fa51840a465cc36a71
Andrew Stanford-Jason <andrew@xmos.com>
2018-09-25 12:03:23 +0100

    updated python_setup

--- python/python_setup.sh
+++ python/python_setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export PYTHONPATH=`pwd`:`dirname $BASH_SOURCE`:`dirname $BASH_SOURCE`/../../lib_vad/python:`dirname $BASH_SOURCE`/../../lib_aec/python:`dirname $BASH_SOURCE`/../../lib_beamsteering/python:$PYTHONPATH
+export PYTHONPATH=`pwd`:`dirname $BASH_SOURCE`:`dirname $BASH_SOURCE`/../../lib_vad/python:`dirname $BASH_SOURCE`/../../lib_aec/python:`dirname $BASH_SOURCE`/../../lib_beamsteering/python:`dirname $BASH_SOURCE`/../../lib_interference_canceller/python:`dirname $BASH_SOURCE`/../../lib_noise_suppression/python:`dirname $BASH_SOURCE`/../../lib_agc/python:$PYTHONPATH
