86431a78e855431d9f25770c90ffb9c09eea3e59
Aaron Stewart <aaron.stewart@xmos.com>
2019-05-16 15:08:24 -0400

    Changed the frame_file API and file format.

--- audio_test_tools/api/frame_file.h
+++ audio_test_tools/api/frame_file.h
@@ -12,38 +12,119 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define ATT_FF_SCOPE_STACK_SIZE(MAX_DEPTH) (sizeof(char*)*(MAX_DEPTH))
+#define ATT_FF_INDEX_STACK_SIZE(MAX_DEPTH) (sizeof(int)*(MAX_DEPTH))
+
 #ifdef __XC__
 extern "C" {
 #endif
 
-FILE *att_ff_open(
-        const char *filename,
-        const char *testname);
+typedef struct {
+    FILE * file;
+    char ** scope_stack;
+    int* index_stack;
+    unsigned max_scope_depth;
+    int current_depth;
+} att_ff_context_t;
 
-void att_ff_close(
-        FILE *ff);
+typedef enum {
+    ATT_FF_NO_INDEX = -1,
+    ATT_FF_INDEXED = 0,
+} att_ff_index_type_t;
 
-void att_ff_frame_start(
-        FILE *ff,
-        unsigned int frame_num);
-
-void att_ff_frame_end(
-        FILE *ff);
 
 typedef enum {
     att_ff_type_s16,
     att_ff_type_u16,
     att_ff_type_s32,
     att_ff_type_u32,
-    att_ff_type_float
+    att_ff_type_vtb_float,
+    att_ff_type_double,
+    att_ff_type_complex_fp,
+    att_ff_type_double_pair_ch_a,
+    att_ff_type_double_pair_ch_b,
 } att_ff_type_t;
 
-void att_ff_array_write(
-        FILE *ff,
-        const char *array_name,
-        void *a,
-        att_ff_type_t type,
-        unsigned int len);
+unsigned att_ff_open(
+        att_ff_context_t* ctx,
+        char** scope_stack,
+        int* index_stack,
+        const char max_scope_depth,
+        const char *filename);
+
+void att_ff_close(
+        att_ff_context_t* ctx);
+
+void att_ff_scope_push(
+        att_ff_context_t* ctx,
+        char* scope_name,
+        const att_ff_index_type_t index_type);
+
+void att_ff_scope_pop(
+        att_ff_context_t* ctx);
+
+void att_ff_index_set(
+        att_ff_context_t* ctx,
+        const int index);
+
+void att_ff_index_increment(
+        att_ff_context_t* ctx);
+
+void att_ff_write_int(
+        const att_ff_context_t* ctx,
+        const char* name,
+        const int value);
+
+void att_ff_write_double(
+        const att_ff_context_t* ctx,
+        const char* name,
+        const double value);
+
+void att_ff_write_scalar(
+        const att_ff_context_t* ctx,
+        const char* name,
+        const void* value,
+        const att_ff_type_t type);
+
+void att_ff_write_vector(
+        const att_ff_context_t* ctx,
+        const char* vec_name,
+        const void* vector,
+        const att_ff_type_t type,
+        const unsigned len);
+
+void att_ff_write_ndim_matrix(
+        const att_ff_context_t* ctx,
+        const char* mat_name,
+        const void* matrix,
+        const int* dimensions,
+        const int dimension_count,
+        const att_ff_type_t type);
+
+        
+void att_ff_write_scalar_indexed(
+        const att_ff_context_t* ctx,
+        const char* name,
+        const void* value,
+        const att_ff_type_t type,
+        const int index);
+
+void att_ff_write_vector_indexed(
+        const att_ff_context_t* ctx,
+        const char* vec_name,
+        const void* vector,
+        const att_ff_type_t type,
+        const unsigned len,
+        const int index);
+
+void att_ff_write_ndim_matrix_indexed(
+        const att_ff_context_t* ctx,
+        const char* mat_name,
+        const void* matrix,
+        const int* dimensions,
+        const int dimension_count,
+        const att_ff_type_t type,
+        const int index);
 
 #ifdef __XC__
 }
