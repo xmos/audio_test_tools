7b718dedfe7648ed545c9f144fc66e929e026362
Luciano Martin <lucianom@xmos.com>
2019-06-04 12:27:56 +0100

    Add functions to convert json to header files

--- python/common_utils.py
+++ python/common_utils.py
@@ -1,15 +1,10 @@
 # Copyright (c) 2019, XMOS Ltd, All rights reserved
 """ This module contains common util functions used by all the VTB libs """
-
 from __future__ import division
 from __future__ import print_function
-
-import os
 import re
-import configparser
-import ast
-import numpy as np
 import json
+import numpy as np
 
 def json_to_dict(config_file, print_param=False):
     datastore = None
@@ -27,19 +22,38 @@ def dict_to_json(config_dict, config_file, print_param=False):
         f.write(json_dump)
         f.close()
 
+def json_to_header_params(module_name, elem_dict, header_file_name, print_param):
+    with open(header_file_name, 'w') as f:
+        header_file_underscore = header_file_name.replace('.', '_')
+        f.write('#ifndef {}\n#define {}\n\n'.format(header_file_underscore, header_file_underscore))
+        for key in elem_dict:
+            if isinstance(elem_dict[key], dict) is False:
+                if print_param:
+                    print("#define {}_{} ({})".format(module_name.upper(),
+                                                      key.upper(), elem_dict[key]))
+                f.write("#define {}_{} ({})\n".format(module_name.upper(),
+                                                      key.upper(), elem_dict[key]))
+            else:
+                header_sub_file = key+'.h'
+                sub_module_name = module_name+'_'+key.replace('_conf', '').\
+                                  replace('_parameters', '')
+                json_to_header_params(sub_module_name,
+                                      elem_dict[key], header_sub_file, print_param)
+        f.write('\n#endif // {}\n'.format(header_file_underscore))
+
+def json_to_header_file(config_file, header_file='', print_param=False):
+    if header_file is '':
+        header_file = config_file.replace('.json', '.h')
+    datastore = json_to_dict(config_file)
+    json_to_header_params(datastore['algo_name'], datastore, header_file, print_param)
 
 def select_process_channels(y_wav_data, channels_to_process):
-
     if channels_to_process == None:
         y_channel_count = len(y_wav_data)
-
     else:
         channels_to_process = np.asarray(channels_to_process)
-        channels_to_process = channels_to_process[(channels_to_process  >= 0) & (channels_to_process < len(y_wav_data))]
-        y_channel_count = min( len(y_wav_data), len(channels_to_process))
-
+        channels_to_process = channels_to_process[(channels_to_process >= 0) &\
+                             (channels_to_process < len(y_wav_data))]
+        y_channel_count = min(len(y_wav_data), len(channels_to_process))
         y_wav_data = y_wav_data[channels_to_process]
-
     return y_wav_data, y_channel_count
-
-
