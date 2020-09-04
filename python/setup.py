64021af05d638897f06587ffc80042f1a0c9f42a
mbanth <mbanth@animus-opus.com>
2020-09-04 11:06:41 +0100

    Update comments

--- python/setup.py
+++ python/setup.py
@@ -5,9 +5,9 @@ import setuptools
 # procedure to set up a suitable python environment for that repository may
 # pip-install this one as editable using this setup.py file.  To minimise the
 # chance of version conflicts while ensuring a minimal degree of conformity,
-# the modules listed here require the same major version and at least the same
-# minor version as specified in the requirements.txt file.  The same modules
-# should appear in the requirements.txt file as given below.
+# the 3rd-party modules listed here require the same major version and at
+# least the same minor version as specified in the requirements.txt file.
+# The same modules should appear in the requirements.txt file as given below.
 setuptools.setup(
     name='audio_test_tools',
     packages=setuptools.find_packages(),
