4a6544b992bae62058a6a7b6afe28534e0e8d815
lucianom <lucianom@xmos.com>
2020-09-21 08:38:51 +0100

    Use numpy version 1.18.5

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     install_requires=[
         'flake8~=3.8',
         'matplotlib~=3.3',
-        'numpy~=1.19',
+        'numpy~=1.18',
         'pandas~=1.1',
         'pylint~=2.5',
         'pytest~=6.0',
